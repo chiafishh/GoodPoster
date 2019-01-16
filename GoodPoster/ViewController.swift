06a782902e328b1423b47e92863b7038379c197d
User02 <user02@user02de-imac.local>
2019-01-16 20:20:10 +0800

    Initial Commit

---
+++ GoodPoster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodPoster
+//
+//  Created by User02 on 2019/1/16.
+//  Copyright © 2019 Chiafishh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
